ea3a6856d1b2bef32445259a1e38a1490ffe0298
Guizinduarrancadiu <guilherme.mgomes@eaportal.org>
2024-04-06 18:25:37 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import Luxuosos from "./components/Luxuosos"
 
 export default function App() {
   return (
-    <main className=" min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
-      <section className="lg:flex max-w-[920px]">
+    <main className="min-h-svh bg-offwhitebg py-20 px-6 antialiased lg:flex justify-center items-center">
+      <div className="lg:flex max-w-[920px]">
         <Sedans/>
         <SUVs/>
         <Luxuosos/>
-      </section>
+      </d>
     </main>
   )
 }
--- src/components/SUVs.jsx
+++ src/components/SUVs.jsx
@@ -2,11 +2,11 @@ import iconSUVs from "../assets/icon-suvs.svg"
 
 export default function SUVs() {
   return (
-    <div className="bg-green p-12">
+    <section className="bg-green p-12">
         <img src={iconSUVs} alt="icone dos suv" className="mb-7" />
         <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">SUVs</h2>
         <p className="font-lexend mb-7 text-offwhite lg:mb-20">Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
-        <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg"href="https://www.webmotors.com.br/suvs/carros/estoque?tipoveiculo=carros&necessidade=SUVs" target="_blank">Ver mais</a>
-    </div>
+        <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-green border-2 border-offwhitebg hover:bg-green hover:text-offwhitebg" href="https://www.webmotors.com.br/suvs/carros/estoque?tipoveiculo=carros&necessidade=SUVs" target="_blank">Ver mais</a>
+    </section>
   )
 }
